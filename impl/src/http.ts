c44ccaaaa1ec4acc277bdd5f7f11a1e55a06bb57
Andrew Paseltiner <apaseltiner@google.com>
2025-12-02 10:14:07 -0500

    Implement Measure-Conversion parsing in simulator

--- impl/src/http.ts
+++ impl/src/http.ts
@@ -1,4 +1,7 @@
-import type { AttributionImpressionOptions } from "./index";
+import type {
+  AttributionConversionOptions,
+  AttributionImpressionOptions,
+} from "./index";
 
 import type { BareItem, Dictionary, Item } from "structured-headers";
 
@@ -43,10 +46,27 @@ function get32BitUnsignedInteger(
   return value;
 }
 
-function parseInnerListOfSites(
+function getPositive32BitUnsignedInteger(
   dict: Dictionary,
   key: string,
-): string[] | undefined {
+): number | undefined {
+  const value = get32BitUnsignedInteger(dict, key);
+  if (value === undefined) {
+    return value;
+  }
+
+  if (value === 0) {
+    throw new RangeError(`${key} must be positive`);
+  }
+
+  return value;
+}
+
+function parseInnerList<T>(
+  dict: Dictionary,
+  key: string,
+  parseItem: (i: number, value: BareItem) => T,
+): T[] | undefined {
   const values = get(dict, key);
   if (values === undefined) {
     return values;
@@ -56,14 +76,48 @@ function parseInnerListOfSites(
     throw new TypeError(`${key} must be an inner list`);
   }
 
-  const sites = [];
+  const result = [];
   for (const [i, [value]] of values.entries()) {
+    result.push(parseItem(i, value));
+  }
+  return result;
+}
+
+function parseInnerListOfSites(
+  dict: Dictionary,
+  key: string,
+): string[] | undefined {
+  return parseInnerList(dict, key, (i, value) => {
     if (typeof value !== "string") {
       throw new TypeError(`${key}[${i}] must be a string`);
     }
-    sites.push(value);
+    return value;
+  });
+}
+
+function validate32BitUnsignedInteger(
+  value: BareItem | Item[] | undefined,
+  errPrefix: string,
+): asserts value is number {
+  if (
+    typeof value !== "number" ||
+    !Number.isInteger(value) ||
+    value < 0 ||
+    value > MAX_UINT32
+  ) {
+    throw new RangeError(
+      `${errPrefix} must be an integer in the 32-bit unsigned range`,
+    );
   }
-  return sites;
+}
+
+function validatePositiveInteger(
+  value: BareItem | Item[] | undefined,
+  errPrefix: string,
+): asserts value is number {
+  if (typeof value !== "number" || !Number.isInteger(value) || value <= 0) {
+    throw new RangeError(`${errPrefix} must be a positive integer`);
+  }
 }
 
 export function parseSaveImpressionHeader(
@@ -97,4 +151,85 @@ export function parseSaveImpressionHeader(
   }
 
   return opts;
+}
+
+export type ParsedMeasureConversionHeader = [
+  opts: AttributionConversionOptions,
+  reportUrl: URL,
+];
+
+export function parseMeasureConversionHeader(
+  input: string,
+  baseUrl: URL,
+): ParsedMeasureConversionHeader {
+  const dict = parseDictionary(input);
+
+  const aggregationService = get(dict, "aggregation-service");
+  if (aggregationService === undefined) {
+    throw new TypeError("aggregation-service is required");
+  }
+  if (typeof aggregationService !== "string") {
+    throw new TypeError("aggregation-service must be a string");
+  }
+
+  const histogramSize = get32BitUnsignedInteger(dict, "histogram-size");
+  if (histogramSize === undefined) {
+    throw new TypeError("histogram-size is required");
+  }
+  if (histogramSize === 0) {
+    throw new RangeError("histogram-size must be positive");
+  }
+
+  const reportUrlString = get(dict, "report-url");
+  if (reportUrlString === undefined) {
+    throw new TypeError("report-url is required");
+  }
+  if (typeof reportUrlString !== "string") {
+    throw new TypeError("report-url must be a string");
+  }
+  const reportUrl = new URL(reportUrlString, baseUrl);
+  // The specification requires reportUrl to be potentially trustworthy, but
+  // there is no direct analogue of this in JS, so for now we let the protocol
+  // check below suffice.
+  if (reportUrl.protocol !== "https:") {
+    throw new TypeError("report-url's scheme must be https");
+  }
+
+  const opts: AttributionConversionOptions = {
+    aggregationService,
+    histogramSize,
+  };
+
+  const epsilon = get(dict, "epsilon");
+  if (epsilon !== undefined && typeof epsilon !== "number") {
+    throw new RangeError("epsilon must be a decimal or an integer");
+  }
+  opts.epsilon = epsilon;
+
+  const lookbackDays = get(dict, "lookback-days");
+  if (lookbackDays !== undefined) {
+    validatePositiveInteger(lookbackDays, "lookback-days");
+  }
+  opts.lookbackDays = lookbackDays;
+
+  opts.matchValues = parseInnerList(dict, "match-values", (i, value) => {
+    validate32BitUnsignedInteger(value, `match-values[${i}]`);
+    return value;
+  });
+
+  opts.impressionSites = parseInnerListOfSites(dict, "impression-sites");
+  opts.impressionCallers = parseInnerListOfSites(dict, "impression-callers");
+
+  opts.credit = parseInnerList(dict, "credit", (i, value) => {
+    if (typeof value !== "number") {
+      throw new RangeError(`credit[${i}] must be a decimal or an integer`);
+    }
+    return value;
+  });
+
+  opts.value = getPositive32BitUnsignedInteger(dict, "value");
+
+  opts.maxValue = getPositive32BitUnsignedInteger(dict, "max-value");
+
+  return [opts, reportUrl];
 }
