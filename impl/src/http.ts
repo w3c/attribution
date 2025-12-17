e7c0683a67c6c33ef30c24e8fad339968167a0ad
Andrew Paseltiner <apaseltiner@google.com>
2025-12-17 09:08:41 -0500

    Parsing helpers

--- impl/src/http.ts
+++ impl/src/http.ts
@@ -17,107 +17,110 @@ function get(dict: Dictionary, key: string): BareItem | Item[] | undefined {
   return value;
 }
 
-function getInteger(dict: Dictionary, key: string): number | undefined {
+function optional<T>(
+  dict: Dictionary,
+  key: string,
+  f: (value: BareItem | Item[], errPrefix: string) => T,
+): T | undefined {
+  const value = get(dict, key);
+  return value === undefined ? value : f(value, key);
+}
+
+function required<T>(
+  dict: Dictionary,
+  key: string,
+  f: (value: BareItem | Item[], errPrefix: string) => T,
+): T {
   const value = get(dict, key);
   if (value === undefined) {
-    return value;
+    throw new TypeError(`${key} is required`);
   }
+  return f(value, key);
+}
 
+function asInteger(value: BareItem | Item[], errPrefix: string): number {
   if (typeof value !== "number" || !Number.isInteger(value)) {
-    throw new TypeError(`${key} must be an integer`);
+    throw new TypeError(`${errPrefix} be an integer`);
   }
-
   return value;
 }
 
-function get32BitUnsignedInteger(
-  dict: Dictionary,
-  key: string,
-): number | undefined {
-  const value = getInteger(dict, key);
-  if (value === undefined) {
-    return value;
+function asDecimalOrInteger(
+  value: BareItem | Item[],
+  errPrefix: string,
+): number {
+  if (typeof value !== "number") {
+    throw new TypeError(`${errPrefix} must be a decimal or an integer`);
   }
-
-  if (value < 0 || value > MAX_UINT32) {
-    throw new RangeError(`${key} must be in the 32-bit unsigned range`);
-  }
-
   return value;
 }
 
-function getPositive32BitUnsignedInteger(
-  dict: Dictionary,
-  key: string,
-): number | undefined {
-  const value = get32BitUnsignedInteger(dict, key);
-  if (value === undefined) {
-    return value;
+function as32BitUnsignedInteger(
+  value: BareItem | Item[],
+  errPrefix: string,
+): number {
+  const integer = asInteger(value, errPrefix);
+  if (integer < 0 || integer > MAX_UINT32) {
+    throw new RangeError(`${errPrefix} must be in the 32-bit unsigned range`);
   }
+  return integer;
+}
 
-  if (value === 0) {
-    throw new RangeError(`${key} must be positive`);
+function as32BitSignedInteger(
+  value: BareItem | Item[],
+  errPrefix: string,
+): number {
+  const integer = asInteger(value, errPrefix);
+  if (integer < MIN_INT32 || integer > MAX_INT32) {
+    throw new RangeError(`${errPrefix} must be in the 32-bit signed range`);
   }
+  return integer;
+}
 
+function asPositive(value: number, errPrefix: string): number {
+  if (value <= 0) {
+    throw new TypeError(`${errPrefix} be positive`);
+  }
   return value;
 }
 
-function parseInnerList<T>(
-  dict: Dictionary,
-  key: string,
-  parseItem: (i: number, value: BareItem) => T,
-): T[] | undefined {
-  const values = get(dict, key);
-  if (values === undefined) {
-    return values;
-  }
+function asPositiveInteger(
+  value: BareItem | Item[],
+  errPrefix: string,
+): number {
+  return asPositive(asInteger(value, errPrefix), errPrefix);
+}
 
+function asPositive32BitUnsignedInteger(
+  value: BareItem | Item[],
+  errPrefix: string,
+): number {
+  return asPositive(as32BitUnsignedInteger(value, errPrefix), errPrefix);
+}
+
+function asString(value: BareItem | Item[], errPrefix: string): string {
+  if (typeof value !== "string") {
+    throw new TypeError(`${errPrefix} must be a string`);
+  }
+  return value;
+}
+
+function asInnerList<T>(
+  values: BareItem | Item[],
+  errPrefix: string,
+  parseItem: (value: BareItem, errPrefix: string) => T,
+): T[] {
   if (!Array.isArray(values)) {
-    throw new TypeError(`${key} must be an inner list`);
+    throw new TypeError(`${errPrefix} must be an inner list`);
   }
-
-  const result = [];
-  for (const [i, [value]] of values.entries()) {
-    result.push(parseItem(i, value));
-  }
-  return result;
+  return values.map(([value], i) => parseItem(value, `${errPrefix}[${i}]`));
 }
 
-function parseInnerListOfSites(
-  dict: Dictionary,
-  key: string,
-): string[] | undefined {
-  return parseInnerList(dict, key, (i, value) => {
-    if (typeof value !== "string") {
-      throw new TypeError(`${key}[${i}] must be a string`);
-    }
-    return value;
-  });
-}
-
-function validate32BitUnsignedInteger(
-  value: BareItem | Item[] | undefined,
+function asInnerListOfStrings(
+  values: BareItem | Item[],
   errPrefix: string,
-): asserts value is number {
-  if (
-    typeof value !== "number" ||
-    !Number.isInteger(value) ||
-    value < 0 ||
-    value > MAX_UINT32
-  ) {
-    throw new RangeError(
-      `${errPrefix} must be an integer in the 32-bit unsigned range`,
-    );
-  }
-}
-
-function validatePositiveInteger(
-  value: BareItem | Item[] | undefined,
-  errPrefix: string,
-): asserts value is number {
-  if (typeof value !== "number" || !Number.isInteger(value) || value <= 0) {
-    throw new RangeError(`${errPrefix} must be a positive integer`);
-  }
+): string[] {
+  return asInnerList(values, errPrefix, asString);
 }
 
 export function parseSaveImpressionHeader(
@@ -125,32 +128,18 @@ export function parseSaveImpressionHeader(
 ): AttributionImpressionOptions {
   const dict = parseDictionary(input);
 
-  const histogramIndex = get32BitUnsignedInteger(dict, "histogram-index");
-  if (histogramIndex === undefined) {
-    throw new TypeError("histogram-index is required");
-  }
-
-  const opts: AttributionImpressionOptions = { histogramIndex };
-
-  opts.conversionSites = parseInnerListOfSites(dict, "conversion-sites");
-  opts.conversionCallers = parseInnerListOfSites(dict, "conversion-callers");
-
-  opts.matchValue = get32BitUnsignedInteger(dict, "match-value");
-
-  opts.lifetimeDays = getInteger(dict, "lifetime-days");
-  if (opts.lifetimeDays !== undefined && opts.lifetimeDays <= 0) {
-    throw new RangeError("lifetime-days must be positive");
-  }
-
-  opts.priority = getInteger(dict, "priority");
-  if (
-    opts.priority !== undefined &&
-    (opts.priority < MIN_INT32 || opts.priority > MAX_INT32)
-  ) {
-    throw new RangeError("priority must be in the 32-bit signed range");
-  }
-
-  return opts;
+  return {
+    histogramIndex: required(dict, "histogram-index", as32BitUnsignedInteger),
+    conversionSites: optional(dict, "conversion-sites", asInnerListOfStrings),
+    conversionCallers: optional(
+      dict,
+      "conversion-callers",
+      asInnerListOfStrings,
+    ),
+    matchValue: optional(dict, "match-value", as32BitUnsignedInteger),
+    lifetimeDays: optional(dict, "lifetime-days", asPositiveInteger),
+    priority: optional(dict, "priority", as32BitSignedInteger),
+  };
 }
 
 export type ParsedMeasureConversionHeader = [
@@ -164,68 +153,41 @@ export function parseMeasureConversionHeader(
 ): ParsedMeasureConversionHeader {
   const dict = parseDictionary(input);
 
-  const aggregationService = get(dict, "aggregation-service");
-  if (aggregationService === undefined) {
-    throw new TypeError("aggregation-service is required");
-  }
-  if (typeof aggregationService !== "string") {
-    throw new TypeError("aggregation-service must be a string");
-  }
-
-  const histogramSize = getPositive32BitUnsignedInteger(dict, "histogram-size");
-  if (histogramSize === undefined) {
-    throw new TypeError("histogram-size is required");
-  }
-
-  const reportUrlString = get(dict, "report-url");
-  if (reportUrlString === undefined) {
-    throw new TypeError("report-url is required");
-  }
-  if (typeof reportUrlString !== "string") {
-    throw new TypeError("report-url must be a string");
-  }
-  const reportUrl = new URL(reportUrlString, baseUrl);
-  // The specification requires reportUrl to be potentially trustworthy, but
-  // there is no direct analogue of this in JS, so for now we let the protocol
-  // check below suffice.
-  if (reportUrl.protocol !== "https:") {
-    throw new TypeError("report-url's scheme must be https");
-  }
-
-  const opts: AttributionConversionOptions = {
-    aggregationService,
-    histogramSize,
-  };
-
-  const epsilon = get(dict, "epsilon");
-  if (epsilon !== undefined && typeof epsilon !== "number") {
-    throw new TypeError("epsilon must be a decimal or an integer");
-  }
-  opts.epsilon = epsilon;
-
-  const lookbackDays = get(dict, "lookback-days");
-  if (lookbackDays !== undefined) {
-    validatePositiveInteger(lookbackDays, "lookback-days");
-  }
-  opts.lookbackDays = lookbackDays;
-
-  opts.matchValues = parseInnerList(dict, "match-values", (i, value) => {
-    validate32BitUnsignedInteger(value, `match-values[${i}]`);
-    return value;
-  });
-
-  opts.impressionSites = parseInnerListOfSites(dict, "impression-sites");
-  opts.impressionCallers = parseInnerListOfSites(dict, "impression-callers");
-
-  opts.credit = parseInnerList(dict, "credit", (i, value) => {
-    if (typeof value !== "number") {
-      throw new RangeError(`credit[${i}] must be a decimal or an integer`);
+  const reportUrl = required(dict, "report-url", (value, errPrefix) => {
+    const url = new URL(asString(value, errPrefix), baseUrl);
+    // The specification requires reportUrl to be potentially trustworthy, but
+    // there is no direct analogue of this in JS, so for now we let the protocol
+    // check below suffice.
+    if (url.protocol !== "https:") {
+      throw new TypeError(`${errPrefix}'s scheme must be https`);
     }
-    return value;
+    return url;
   });
 
-  opts.value = getPositive32BitUnsignedInteger(dict, "value");
-  opts.maxValue = getPositive32BitUnsignedInteger(dict, "max-value");
+  const opts = {
+    aggregationService: required(dict, "aggregation-service", asString),
+    histogramSize: required(
+      dict,
+      "histogram-size",
+      asPositive32BitUnsignedInteger,
+    ),
+    epsilon: optional(dict, "epsilon", asDecimalOrInteger),
+    lookbackDays: optional(dict, "lookback-days", asPositiveInteger),
+    matchValues: optional(dict, "match-values", (values, errPrefix) =>
+      asInnerList(values, errPrefix, as32BitUnsignedInteger),
+    ),
+    impressionSites: optional(dict, "impression-sites", asInnerListOfStrings),
+    impressionCallers: optional(
+      dict,
+      "impression-callers",
+      asInnerListOfStrings,
+    ),
+    credit: optional(dict, "credit", (values, errPrefix) =>
+      asInnerList(values, errPrefix, asDecimalOrInteger),
+    ),
+    value: optional(dict, "value", asPositive32BitUnsignedInteger),
+    maxValue: optional(dict, "max-value", asPositive32BitUnsignedInteger),
+  };
 
   return [opts, reportUrl];
 }
